c9602d87ceab653b36d98a49c47a16d9e684ec85
amir mashayekhi <amirmahsayekhi084@gmail.com>
2025-11-10 13:01:15 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+for( let i = 1; i <= 20 ; i++){
+   if(i % 3 === 0 && i % 5 ===0)
+   {
+      console.log(i, "coin tarp");
+
+   }else if(i % 5 === 0)
+   {console.log (i , "trap");}
+   else if ( i % 3 === 0)
+   {console.log (i, "coin")}
+}
