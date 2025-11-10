cb0dd51d4628a0294bd0378a32e4299e5bafec8b
amir mashayekhi <amirms88@outlook.com>
2025-11-10 18:40:57 +0330

    separate logic into multiple IFs

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ for( let i = 1; i <= 20 ; i++){
 
    } if(i % 3 === 0)
    {console.log (i , "coin");}
-   else if ( i % 3 === 0 && i % 5 === 0)
+    if ( i % 3 === 0 && i % 5 === 0)
    {console.log (i, "cointrap")}
 }
